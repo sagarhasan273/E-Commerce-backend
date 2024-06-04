8c9a1cfacca46e396cae7167a66f2ef1d37ab253
sagarhasan273 <sagarhasan273@gmail.com>
2024-06-05 05:23:06 +0600

    signup api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+require('./db/config');
+const users = require('./db/Users');
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send("app is working...");
+
+app.post("/register", (req, res) => {
+    res.send("Api is working.");
+    console.log(users);
 })
 
 app.listen(5000)
